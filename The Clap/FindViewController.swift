3e7f933d80e43f2229f23fe48bb16cd3f4b4904b
alexpopov <hello@alexpopov.ca>
2016-02-27 19:20:15 -0800

    tried pushing?

--- The Clap/FindViewController.swift
+++ The Clap/FindViewController.swift
@@ -7,30 +7,27 @@
 //
 
 import UIKit
+import Prelude
 
 class FindViewController: BaseViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  let tableView = TableView()
 
-        // Do any additional setup after loading the view.
-      navigationItem.title = "Find Tournaments"
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    // Do any additional setup after loading the view.
+    navigationItem.title = "Find Tournaments"
 
-    /*
-    // MARK: - Navigation
+  }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  func setupTableView() -> TableView {
+    fatalError()
+  }
 
 }
